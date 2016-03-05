49dbc13b878d440b26ba364391fcc0c238f8eb26
yan wang <tianya_wy@hotmailcom>
2016-03-06 00:22:27 +0800

    init project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to go-visual
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/go_visual/core_test.clj
@@ -0,0 +1,7 @@
+(ns go-visual.core-test
+  (:require [clojure.test :refer :all]
+            [go-visual.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
