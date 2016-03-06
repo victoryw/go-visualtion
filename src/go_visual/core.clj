8f669db635b33a12782265b333e6f5428e806493
yan wang <tianya_wy@hotmailcom>
2016-03-06 19:55:23 +0800

    [victoryw] #1 init get one pipeline instance status

--- src/go_visual/core.clj
+++ src/go_visual/core.clj
@@ -1,7 +1,32 @@
 (ns go-visual.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clj-http.client :as client])
+  (:require [clojure.data.json :as json]))
+
+(defn extract-pipeline-instance-history 
+  [pipeline]
+  {:name (:name pipeline) 
+   :counter (:counter pipeline)
+   :statges (map 
+             (fn [stages-map] {:name (get stages-map :name) :counter (get stages-map :counter)}) 
+             (get pipeline :stages))})
+
+(defn fetch-pipeline-instance
+  [url username password]
+  (json/read-str (get (client/get url {:basic-auth [username password]}) :body)
+                 :key-fn keyword))
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  [& {:keys [url username password]}]
+
+  (def pipeline-data 
+    ((fn [parms] 
+       (extract-pipeline-instance-history  
+        (fetch-pipeline-instance 
+         url 
+         username 
+         password))) 
+     nil))
+  (println pipeline-data))
+
