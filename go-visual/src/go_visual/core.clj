f83d169ff12e13ac066dcf75594bd656d2475ed0
yan wang <tianya_wy@hotmailcom>
2016-03-07 16:24:29 +0800

    [victoryw] #N/A copy statis data to site folder

--- go-visual/src/go_visual/core.clj
+++ go-visual/src/go_visual/core.clj
@@ -12,10 +12,16 @@
              (fn [stages-map] {:name (get stages-map :name) :counter (get stages-map :counter)}) 
              (get pipeline :stages))})
 
-(defn fetch-pipeline-instance
+(defn fetch-pipeline-datas
   [url username password]
   (json/read-str (get (client/get url {:basic-auth [username password]}) :body)
                  :key-fn keyword))
+
+(defn write-to-site-json
+  [statis]
+  (spit "../go-visual-site/data.json" (json/write-str
+   {:categories (map #(format "%s-%s" (:name %) (:counter %)) statis)
+    :data (map #(- (:statges-run-times %) (:statges %)) statis)})))
 
 (defn statistic-pipeline-instace
   [pipeline-instance]
@@ -23,17 +29,13 @@
    :counter (:counter pipeline-instance)
    :statges-run-times   (reduce + (map (comp string2number/to-number :counter) (:statges pipeline-instance)))
    :statges ((comp count :statges) pipeline-instance)})
-   
-   
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& {:keys [url username password]}]
   (def statis 
-    ((comp  statistic-pipeline-instace 
-            extract-pipeline-instance-history
-            fetch-pipeline-instance) 
-     url
-     username
-     password))
+    (map (comp  statistic-pipeline-instace 
+                extract-pipeline-instance-history)
+         (take 15 (:pipelines (fetch-pipeline-datas url username password)))))
   (println statis))
 
